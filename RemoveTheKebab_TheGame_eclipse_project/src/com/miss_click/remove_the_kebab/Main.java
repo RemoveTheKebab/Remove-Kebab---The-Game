a79ecdbcc2c555287ffd549035ac95bff7ef1edb
RemoveTheKebab <RmKebabTheGame@gmail.com>
2016-03-21 13:32:29 +0100

    Star

---
+++ RemoveTheKebab_TheGame_eclipse_project/src/com/miss_click/remove_the_kebab/Main.java
@@ -0,0 +1,20 @@
+package com.miss_click.remove_the_kebab;
+
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	private JFrame frame;
+	private Canvas canvas;
+	
+	public Main(){
+		
+	}
+	
+	public static void main(String[] args){
+		new Main();
+	}
+	
+}
